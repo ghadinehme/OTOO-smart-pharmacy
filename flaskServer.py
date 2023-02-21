9016ff91e95d21e29e813a974168281a085b417c
Ben Randoing <benjamin.randoing@duke.edu>
2023-02-20 21:21:22 -0800

    Implemented Flask Form for Order

--- flaskServer.py
+++ flaskServer.py
@@ -3,27 +3,40 @@
 # Created Ben Randoing on 02/12/2023
 
 from flask import Flask, jsonify, request, render_template
-from helper import recipes, descriptions, ingredients, instructions
+from helper import medications, descriptions, ingredients, instructions
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, SelectField, IntegerRangeField
 import json
 import numpy as np
 import sqlalchemy
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecret'
+
+class OrderForm(FlaskForm):
+    med = SelectField("Medication", coerce=int,
+                      choices = [(1, "Tylenol"),
+                                 (2, "Ibuprofen"),
+                                 (3, "Aleve")])
+    quantity = IntegerRangeField("Quantity")
+    submit = SubmitField('Submit Order')
 
 @app.route("/")
 def index():
-  return render_template("index.html", template_recipes=recipes)
+    order_form = OrderForm()
+    return render_template("index.html", template_meds=medications,
+                           template_form=order_form)
 
-@app.route("/recipe/<int:id>", methods=["GET", "POST"])
+@app.route("/med/<int:id>", methods=["GET", "POST"])
 def recipe(id):
-  return render_template("recipe.html", template_recipe=recipes[id],
+    return render_template("med.html", template_med=medications[id],
                          template_description=descriptions[id],
                          template_ingredients=ingredients[id],
                          template_instructions=instructions[id])
 
 @app.route("/about")
 def about():
-  return render_template("about.html")
+    return render_template("about.html")
 
 if __name__ == '__main__':
     app.run()
--- helper.py
+++ helper.py
@@ -1,9 +1,10 @@
-recipes = {1: "fried egg", 2: "buttered toast"}
+medications = {1: "Tylenol", 2: "Ibuprofen", 3: "Aleve"}
 types = {1: "Breakfast", 2: "Breakfast"}
-descriptions = {1: "Egg fried in butter", 2: "Toasted bread spread with butter"}
-ingredients = {1: ["1 pad of butter", "1 Egg", "A pinch of salt"], 2: ["1 pad of salted butter", "1 slice of bread"]}
-instructions = {1: {"Step 2": "Crack the egg into the buttered pan", "Step 5": "Serve egg after about a minute and a half", "Step 1": "Melt butter in pan over medium-low heat", "Step 4": "Flip egg after about a minute and a half", "Step 3": "Sprinke the pinch of salt onto cooking egg",},
-                2: {"Step 3": "Put the pad of butter on the toasted bread", "Step 4": "After a minute spread the melted butter onto the bread", "Step 1": "Put the bread in the toaster", "Step 2": "Take the toast out of the toaster"}}
+descriptions = {1: "Pain Relief", 2: "Pain Relief", 3: "Allergy Relief"}
+ingredients = {1: ["a", "b", "c"], 2: ["a", "b", "c"], 3: ["a", "b", "c"]}
+instructions = {1: {"Step 2": "a", "Step 5": "b", "Step 1": "c", "Step 4": "d", "Step 3": "e"},
+                2: {"Step 3": "a", "Step 4": "b", "Step 1": "c", "Step 2": "d"},
+                3: {"Step 3": "a", "Step 4": "b", "Step 1": "c", "Step 2": "d"}}
 comments = {1: ["Yummy!!", "Egg-cellent ;->"], 2: ["Toasty", "What a great recipe!"]}
 
 def add_ingredients(recipe_id=None, text=None):
