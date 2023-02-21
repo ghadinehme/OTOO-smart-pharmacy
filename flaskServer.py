c64ba96a2a47737a5184942fd8f5aebaff4daada
Ben Randoing <benjamin.randoing@duke.edu>
2023-02-20 19:58:02 -0800

    Added templates for flask server

--- flaskServer.py
+++ flaskServer.py
@@ -2,23 +2,28 @@
 # Additionally must store locations and pills available to dispense
 # Created Ben Randoing on 02/12/2023
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
+from helper import recipes, descriptions, ingredients, instructions
 import json
 import numpy as np
 import sqlalchemy
-import matplotlib
 
 app = Flask(__name__)
 
-test_dict = {'name': 'Ben Randoing'}
+@app.route("/")
+def index():
+  return render_template("index.html", template_recipes=recipes)
 
-@app.route('/')
-@app.route('/home', methods = ["GET", "POST"])
-def home():
-    if request.method == "POST":
-        print(request.get_json())
-    else:
-        return jsonify(test_dict)
+@app.route("/recipe/<int:id>", methods=["GET", "POST"])
+def recipe(id):
+  return render_template("recipe.html", template_recipe=recipes[id],
+                         template_description=descriptions[id],
+                         template_ingredients=ingredients[id],
+                         template_instructions=instructions[id])
+
+@app.route("/about")
+def about():
+  return render_template("about.html")
 
 if __name__ == '__main__':
     app.run()
